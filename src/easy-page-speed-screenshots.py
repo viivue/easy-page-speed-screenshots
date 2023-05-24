d1bb1c9bbe95f644c37352f9f556b180725ffcbe
phongkhuu115 <phongkhuu115@gmail.com>
2023-05-24 18:00:02 +0700

    refactor(general): add description for whole tool

--- src/easy-page-speed-screenshots.py
+++ src/easy-page-speed-screenshots.py
@@ -223,14 +223,14 @@ def epss_send_link_for_test(links):
 # collect user input link
 def epss_user_input():
     print("""
-    Easy Page Speed Screenshot (EPSS)
-    -----------------------------------
-    Bulk save page speed screenshots automatically.
-
-    List of tools supported:
-    • https://pagespeed.web.dev/
-    • https://gtmetrix.com/
-    • https://tools.pingdom.com/
+Easy Page Speed Screenshot (EPSS)
+-----
+Bulk save page speed screenshots automatically.
+List of tools supported:
+• https://pagespeed.web.dev/
+• https://gtmetrix.com/
+• https://tools.pingdom.com/
+-----
     """)
     global INPUT_LINK
     INPUT_LINK = []
